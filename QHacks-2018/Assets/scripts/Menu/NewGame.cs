07c079a86ee804438eefa4083e6c80d7dd6211bb
MichaelRobertNg <michael.99.ng@gmail.com>
2018-02-03 13:59:50 -0500

    working menu

---
+++ QHacks-2018/Assets/scripts/Menu/NewGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGame : MonoBehaviour {
+
+    private void OnMouseDown()
+    {
+        transform.localScale *= 0.9f;
+    }
+    private void OnMouseUp()
+    {
+        Application.LoadLevel(1);
+    }
+}
