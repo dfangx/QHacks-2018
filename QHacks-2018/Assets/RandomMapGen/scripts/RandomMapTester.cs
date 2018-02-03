5a2a3969cdc95063821562969800b38373d5802e
DragonFangXX <cyrus.ng@gmail.com>
2018-02-03 06:50:59 -0500

    Completed random map island

--- QHacks-2018/Assets/RandomMapGen/scripts/RandomMapTester.cs
+++ QHacks-2018/Assets/RandomMapGen/scripts/RandomMapTester.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class RandomMapTester : MonoBehaviour {
+public class RandomMapTester : MonoBehaviour
+{
+
     [Header("Map Dimensions")]
     public int mapWidth = 20;
     public int mapHeight = 20;
 
     [Space]
-    [Header("Visualize Map")]
+    [Header("Vizualize Map")]
     public GameObject mapContainer;
     public GameObject tilePrefab;
     public Vector2 tileSize = new Vector2(16, 16);
@@ -17,9 +18,28 @@ public class RandomMapTester : MonoBehaviour {
     [Header("Map Sprites")]
     public Texture2D islandTexture;
 
+    [Space]
+    [Header("Decorate Map")]
+    [Range(0, .9f)]
+    public float erodePercent = .5f;
+    public int erodeIterations = 2;
+    [Range(0, .9f)]
+    public float treePercent = .3f;
+    [Range(0, .9f)]
+    public float hillPercent = .2f;
+    [Range(0, .9f)]
+    public float mountainPercent = .1f;
+    [Range(0, .9f)]
+    public float townPercent = .05f;
+    [Range(0, .9f)]
+    public float monsterPercent = .1f;
+    [Range(0, .9f)]
+    public float lakePercent = .05f;
+
     public Map map;
 
-    private void Start()
+    // Use this for initialization
+    void Start()
     {
         map = new Map();
     }
@@ -27,14 +47,24 @@ public class RandomMapTester : MonoBehaviour {
     public void MakeMap()
     {
         map.NewMap(mapWidth, mapHeight);
-        Debug.Log("Created a new " + map.columns + "x" + map.rows + " map");
+        map.CreateIsland(
+            erodePercent,
+            erodeIterations,
+            treePercent,
+            hillPercent,
+            mountainPercent,
+            townPercent,
+            monsterPercent,
+            lakePercent
+        );
         CreateGrid();
+        CenterMap(map.castleTile.id);
     }
 
     void CreateGrid()
     {
         ClearMapContainer();
-        Sprite[] sprites = Resources.LoadAll <Sprite>(islandTexture.name);   
+        Sprite[] sprites = Resources.LoadAll<Sprite>(islandTexture.name);
 
         var total = map.tiles.Length;
         var maxColumns = map.columns;
@@ -43,6 +73,7 @@ public class RandomMapTester : MonoBehaviour {
 
         for (var i = 0; i < total; i++)
         {
+
             column = i % maxColumns;
 
             var newX = column * tileSize.x;
@@ -53,20 +84,41 @@ public class RandomMapTester : MonoBehaviour {
             go.transform.SetParent(mapContainer.transform);
             go.transform.position = new Vector3(newX, newY, 0);
 
-            var spriteID = 0;
-            var sr = go.GetComponent<SpriteRenderer>();
-            sr.sprite = sprites[spriteID];
+            var tile = map.tiles[i];
+            var spriteID = tile.autotileID;
+
+            if (spriteID >= 0)
+            {
+                var sr = go.GetComponent<SpriteRenderer>();
+                sr.sprite = sprites[spriteID];
+            }
 
             if (column == (maxColumns - 1))
+            {
                 row++;
+            }
+
         }
+
     }
 
     void ClearMapContainer()
     {
-        var children = mapContainer.transform.GetComponentsInChildren<Transform>();
 
+        var children = mapContainer.transform.GetComponentsInChildren<Transform>();
         for (var i = children.Length - 1; i > 0; i--)
+        {
             Destroy(children[i].gameObject);
+        }
+
+    }
+
+    void CenterMap(int index)
+    {
+        var camPos = Camera.main.transform.position;
+        var width = map.columns;
+        camPos.x = (index % width) * tileSize.x;
+        camPos.y = -((index / width) * tileSize.x);
+        Camera.main.transform.position = camPos;
     }
 }
