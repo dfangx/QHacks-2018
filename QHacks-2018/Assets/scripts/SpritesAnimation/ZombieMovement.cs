18353f61572976727b54859ea6b87d76bbe39432
MichaelRobertNg <michael.99.ng@gmail.com>
2018-02-03 23:46:15 -0500

    zombies

---
+++ QHacks-2018/Assets/scripts/SpritesAnimation/ZombieMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieMovement : MonoBehaviour
+{
+
+    public Transform target;
+    public float moveSpeed = 1;
+
+    void Start()
+    {
+        target = GameObject.Find("Person").transform;
+    }
+
+    void Update()
+    {
+        Chase();
+    }
+
+    void Chase()
+    {
+        Vector3 targetDirection = target.position - transform.position;
+        transform.position += targetDirection * moveSpeed * Time.deltaTime;
+    }
+}
